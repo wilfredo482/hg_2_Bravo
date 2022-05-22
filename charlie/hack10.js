fe15261651f0ceec04d75d3a26c5f927eb179966
unknown <enif.velasquez97@gmail.com>
2022-05-21 21:43:38 -0400

    feat: add charlie hacks

---
+++ charlie/hack10.js
@@ -0,0 +1,5 @@
+let Array1 = [100,200,300,400,500,600,700];
+let Array1_1 = Array1.slice(0,4);
+let Array1_2 = Array1.slice(4,7);
+let arrayPalabras = ["qux","thud"];
+console.log(Array1.concat(arrayPalabras,Array1_2));
---
+++ charlie/hack4.js
@@ -0,0 +1,4 @@
+let Array4 = [100,200,300,400,500,600,700];
+Array4.splice(2,1);
+Array4.splice(3,1);
+console.log(Array4);
