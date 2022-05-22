927b9940b1ecb945325f7f5b69d6fb291508479e
federico98 <geminiskanon.98@gmail.com>
2022-05-21 21:19:35 -0300

    feat:hacks delta

---
+++ delta/hack5.js
@@ -0,0 +1,5 @@
+var array = [100,200,300,400,500,600,700]
+
+let array2 = array.slice(0)
+
+console.log(array2)
---
+++ delta/hack8.js
@@ -0,0 +1,5 @@
+var array = [100,200,300,400,500,600,700]
+
+array.push("delta")
+
+console.log(array)
